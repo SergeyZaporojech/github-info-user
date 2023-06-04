efc1a3121c28e1878db132def8f38d18c42eeafc
SergeyZaporojech <91011052+SergeyZaporojech@users.noreply.github.com>
2023-06-04 11:00:51 +0300

    Inite

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [username, setUsername] = useState('');
+  const [userData, setUserData] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(`https://api.github.com/users/${username}`);
+      if (response.ok) {
+        const data = await response.json();
+        setUserData(data);
+      } else {
+        throw new Error('User not found');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Github User Info</h1>
+      <form className="form" onSubmit={handleSubmit}>
+        <input
+          className="input"
+          type="text"
+          placeholder="Enter GitHub username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <button className="button" type="submit">Get Info</button>
+      </form>
+      {userData && (
+        <div className="info">
+          <h2>{userData.name}</h2>
+          <p>{userData.bio}</p>
+          <p>Followers: {userData.followers}</p>
+          <p>Following: {userData.following}</p>
+          <img src={userData.avatar_url} alt="User Avatar" />
+          <p>Public Repositories: {userData.public_repos}</p>
+          <p>Location: {userData.location}</p>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
